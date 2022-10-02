6b77ffe3810678bc6e8f6ec66c395ab710c6b3f1
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-02 13:49:21 -0300

    Adding the buy function

--- Angular/sm-computers/src/app/carrinho.service.ts
+++ Angular/sm-computers/src/app/carrinho.service.ts
@@ -10,7 +10,7 @@ export class CarrinhoService {
   constructor() { }
 
   obtemCarrinho() {
-   this.itens =JSON.parse(localStorage.getItem("carrinho") || "");
+   this.itens =JSON.parse(localStorage.getItem("carrinho") || "[]");
    return this.itens;
   }
 
--- Angular/sm-computers/src/app/carrinho/carrinho.component.ts
+++ Angular/sm-computers/src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CarrinhoService } from '../carrinho.service';
 import { IProduto, IProdutoCarrinho } from '../produtos';
 
@@ -9,18 +10,32 @@ import { IProduto, IProdutoCarrinho } from '../produtos';
 })
 export class CarrinhoComponent implements OnInit {
   itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
 
   constructor(
-    public carrinhoService: CarrinhoService
+    public carrinhoService: CarrinhoService,
+    private router: Router
   ) { }
     
   ngOnInit(): void {
     this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaTotal();
+  }
+
+  calculaTotal() {
+    this.total = this.itensCarrinho.reduce((prev,curr) => prev + (curr.preco * curr.quantidade), 0);
   }
 
   removerProdutoCarrinho(produtoId : number) {
     this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
     this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calculaTotal();
   }
 
+  comprar() {
+    alert("Parabéns, você finalizou a sua compra!");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
+  
+  }
 }
