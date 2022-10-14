72b35a514d66600cf45b2cf2cb5090a96c279e3c
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-14 10:56:12 -0300

    Learning Less

---
+++ HTML avançado/PWA - Sérgio/public/_js/scripts.js
@@ -0,0 +1,6 @@
+const previousOperationsText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelector("#buttons-container");
+
+console.log(buttons);
+console.log("Entrei aqui");
