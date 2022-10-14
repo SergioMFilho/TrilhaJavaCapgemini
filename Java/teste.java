bebc8d60fbe92bd3a3e1e4579ff516a4e5b39d6e
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-14 18:34:27 -0300

    Creating pages with SASS

---
+++ Java/teste.java
@@ -0,0 +1,15 @@
+import java.io.Console;
+
+public class teste {
+    public static void main(String[] args) {
+        int numero = 10;
+        if(numero => 10)
+        {
+            Console.WriteLine("Maior ou igual a 10");
+        }
+        else
+        {
+            Console.WriteLine("Menor que 10");
+        }
+    }
+}
